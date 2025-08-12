e6d8b98770abbdf92b0f63170776d5d491279e60
TShuri <andr.xti@gmail.com>
2025-08-12 14:00:28 +0800

    Update logger.py

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,16 +1,11 @@
 import logging
-import os
-
-LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), '..', 'logs')
-os.makedirs(LOG_DIR, exist_ok=True)
-LOG_FILE = os.path.join(LOG_DIR, 'app.log')
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
     if not logger.hasHandlers():
         formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s')
-        file_handler = logging.FileHandler(LOG_FILE, encoding='utf-8')
-        file_handler.setFormatter(formatter)
-        logger.addHandler(file_handler)
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
         logger.setLevel(logging.INFO)
     return logger
